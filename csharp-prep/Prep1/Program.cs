fa1f07987cc0203fc71850dbd46954a9fe766239
Justan Ostrich <hannahcren@gmail.com>
2024-06-11 14:33:43 -0600

    Did the James Bond assignment.

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -1,9 +1,17 @@
 using System;
+using System.Runtime.ExceptionServices;
 
 class Program
 {
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Prep1 World!");
+        Console.Write("What is your first name? ");
+        string FirstName = Console.ReadLine();
+        Console.Write("What is your last name? ");
+        string LastName = Console.ReadLine();
+        Console.WriteLine("");
+
+        Console.WriteLine($"Your name is {LastName}, {FirstName} {LastName}. ");
     }
 }
