47a2a49bd497d2ee76ec3e650e5482de99929994
Justan Ostrich <hannahcren@gmail.com>
2024-06-13 15:09:28 -0600

    Did prep 3

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,32 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Random randomGenerator = new Random();
+        int randomNumber = randomGenerator.Next(1, 100);
+        
+        bool numberIsGuessed = false;
+        while (numberIsGuessed == false)
+        {
+
+            // Guess the input from the user
+            Console.Write("What is your guess? ");
+            string input = Console.ReadLine();
+            int guess = int.Parse(input);
+
+            if (guess == randomNumber)
+            {
+                numberIsGuessed = true;
+                Console.WriteLine("You guessed it!");
+            }
+            else if (guess < randomNumber)
+            {
+                Console.WriteLine("Higher");
+            }
+            else
+            {
+                Console.WriteLine("Lower");
+            }
+        }
+
     }
 }
