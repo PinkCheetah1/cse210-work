f2230eb6180479f8b484efdc8005b2c87c014726
Justan Ostrich <hannahcren@gmail.com>
2024-07-08 16:16:37 -0600

    Did stuff and things in class for Sandbox (polymorphism)

---
+++ sandbox/Sandbox/ConstructionWorker.cs
@@ -0,0 +1,12 @@
+public class ConstructionWorker : Employee
+{
+    public ConstructionWorker(string name, double hours, double wage) : base(name, hours, wage)
+    {
+
+    }
+
+    public override double GetPay()
+    {
+        return base.GetPay() + 1000;
+    }
+}
--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Reflection;
 // using System.Security.Cryptography;
 // System.Collections.Generic;
 
@@ -11,10 +12,27 @@ class Program
     {
         Console.WriteLine("Bonjour Tout Le Monde");
 
-        Circle myCircle = new Circle(10);
-        Console.WriteLine($"{myCircle.GetArea()}");
+        Employee employee = new Employee("Bob", 40, 23.77);
 
-        Circle unitCircle = new Circle(1);
-        Console.WriteLine($"{unitCircle.GetCircumference()}");
+        // Console.WriteLine($"{employee.GetName()} is due: {employee.GetPay()}");
+        // Console.WriteLine("Pay him now )=<");
+
+        ConstructionWorker constructionWorker = new ConstructionWorker("Betty", 40, 35.8);
+        // Console.WriteLine($"{constructionWorker.GetName()} is due: {constructionWorker.GetPay()}");
+
+        Doctor doctor = new Doctor("Belinda", 240000.99);
+        // Console.WriteLine($"{doctor.GetName()} is due: {employee.GetPay()}");
+
+
+        List<Employee> employees = new List<Employee>();
+        employees.Add(employee);
+        employees.Add(constructionWorker);
+        employees.Add(doctor);
+
+        foreach (Employee e in employees)
+        {
+            Console.WriteLine($"{e.GetName()} is due: {e.GetPay()}");
+            Console.WriteLine("Pay him now )=<");
+        }
     }
 }
